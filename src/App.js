2bd526459d046168d724662c05b875955c6bcee5
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-08-16 13:47:17 -0500

    here we goooo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import Canvas from './components/Canvas';
+import React, { useState, useRef, useEffect } from 'react';
+import { useInterval } from './useInterval';
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS
+} from './constants';
 
-function App() {
-  return (
-    <div className="App">
-      <Canvas />
-    </div>
-  );
-}
+import './App.css';
+
+const App = () => <div>Snake Game</div>
 
 export default App;
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import { useWindowDimensions } from '../useWindowDimensions';
 
 const Canvas = () => {
-  const { height, width } = useWindowDimensions();
-  
-  const c = canvas.getContext('2d');
-
-  c.fillRect(100, 100, 5, 5);
 
   return (
-    <canvas style={{width: `${width}px`, height: `${height}px`}}>
-      Canvas, here.
+    <canvas>
     </canvas>
   )
 }

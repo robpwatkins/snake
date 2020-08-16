1ec2c21de4ba631d6c2148c482232e29ee75e4eb
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-08-16 13:32:33 -0500

    gettin that custom useInterval hook from DAn

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,6 +3,10 @@ import { useWindowDimensions } from '../useWindowDimensions';
 
 const Canvas = () => {
   const { height, width } = useWindowDimensions();
+  
+  const c = canvas.getContext('2d');
+
+  c.fillRect(100, 100, 5, 5);
 
   return (
     <canvas style={{width: `${width}px`, height: `${height}px`}}>
